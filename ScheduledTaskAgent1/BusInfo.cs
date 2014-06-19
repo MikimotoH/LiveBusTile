3cbad63226f9248a72571af2b95a474eb7bab046
MikimotoH <mikimotoh@gmail.com>
2014-06-19 20:22:21 +0800

    new branch: ListOfBusList
    BusInfo and BusListVM

---
+++ ScheduledTaskAgent1/BusInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduledTaskAgent1
+{
+    public class BusInfo
+    {
+        public string Name{get;set;}
+        public string Station{get;set;}
+        public BusDir Dir { get; set; }
+        public string TimeToArrive { get; set; }
+    }
+
+    public class BusListVM
+    {
+        public string ListName { get; set; }
+        ObservableCollection<BusInfo> Buses { get; set; }
+    }
+
+    public static class Database
+    {
+        public static void SaveBuses()
+        {
+
+        }
+    }
+
+
+}
